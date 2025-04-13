aeba14bd9d30195ad45f63e766f9025178bb63ac
Nauzet Fernández Lorenzo <146053441+NauziX@users.noreply.github.com>
2025-04-13 09:12:12 +0100

    Add files via upload

--- ExamenNauzetTest.scala
+++ ExamenNauzetTest.scala
@@ -7,24 +7,19 @@ class ExamenNauzetTest extends TestInit {
 
   "Ejercicio 1" should "filtrar y ordenar estudiantes con calificacion > 8" in {
     val estudiantes = Seq(
-      ("Luis", 20, 9.5),
-      ("Ana", 22, 6.0),
-      ("Carlos", 25, 10.0)
+      ("Nauzet", 20, 9.5),
+      ("David", 22, 6.0),
+      ("Miguel", 25, 10.0)
     ).toDF("nombre", "edad", "calificacion")
 
     val resultado = ejercicio1(estudiantes)
 
-    resultado.count() shouldBe 2
-    resultado.columns should contain allOf ("nombre", "calificacion")
-    resultado.collect().map(_.getString(0)).toSeq shouldBe Seq("Carlos", "Luis")
+    resultado.collect().map(_.getString(0)).toSeq shouldBe Seq("Miguel", "Nauzet")
   }
 
   "Ejercicio 2" should "añadir columna indicando si el número es par o impar" in {
     val numeros = Seq(1, 2, 3, 4, 5).toDF("numero")
     val resultado = ejercicio2(numeros)(spark)
-
-    resultado.columns should contain allOf ("numero", "Par o Impar")
-    resultado.count() shouldBe 5
 
       val esperado = Map(
         1 -> "es impar",
@@ -43,28 +38,25 @@ class ExamenNauzetTest extends TestInit {
 
   "Ejercicio 3" should "unir y calcular promedio de calificaciones por estudiante" in {
     val estudiantes1 = Seq(
-      (1, "Luis"),
-      (2, "Ana"),
-      (3, "Carlos")
+      (1, "Nauzet"),
+      (2, "Aday"),
+      (3, "Eche")
     ).toDF("id", "nombre")
 
     val calificaciones = Seq(
       (1, "Matemáticas", 8.0),
       (1, "Lengua", 7.0),
       (2, "Matemáticas", 9.0),
-      (3, "Lengua", 6.0),
-      (3, "Historia", 8.0)
+      (2, "Lengua", 9.0),
+      (3, "Matemáticas", 6.0),
+      (3, "Lengua", 8.0)
     ).toDF("id_estudiante", "asignatura", "calificacion")
 
     val resultado = ejercicio3(estudiantes1, calificaciones)
-
-    resultado.columns should contain allOf ("nombre", "promedio")
-    resultado.count() shouldBe 3
-
     val esperado = Map(
-      "Luis" -> 7.5,
-      "Ana" -> 9.0,
-      "Carlos" -> 7.0
+      "Nauzet" -> 7.5,
+      "Aday" -> 9.0,
+      "Eche" -> 7.0
     )
 
     resultado.collect().foreach { fila =>
@@ -91,26 +83,15 @@ class ExamenNauzetTest extends TestInit {
 
 
   "Ejercicio 5" should "calcular ingreso total por producto desde CSV de ventas" in {
-    val ventas = Seq(
-      (1, "A", 2, 10.0),
-      (2, "B", 1, 20.0),
-      (3, "A", 3, 10.0),
-      (4, "C", 5, 5.0)
-    ).toDF("id_venta", "id_producto", "cantidad", "precio_unitario")
+    val ventas = spark.read
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .csv("C:\\Users\\Nauzet\\Scala\\MiPrimerSparkApp\\src\\main\\scala\\app\\data\\ventas.csv")
 
-    val resultado = ejercicio5(ventas)(spark)
+    val out = ejercicio5(ventas)(spark).collect().map(x => (x.getInt(0), x.getDouble(1)))
 
-    val esperado = Map(
-      "A" -> 50.0,
-      "B" -> 20.0,
-      "C" -> 25.0
-    )
+    out.toList shouldBe List((108,486.0), (101,460.0), (103,280.0), (107,396.0), (102,405.0), (109,540.0), (105,570.0), (110,494.0), (106,425.0), (104,800.0))
 
-    val resultadoMap = resultado.collect().map(f => f.getString(0) -> f.getDouble(1)).toMap
-
-    resultado.columns should contain allOf ("id_producto", "total_ingreso")
-    resultado.count() shouldBe 3
-    resultadoMap shouldBe esperado
   }
 
 
